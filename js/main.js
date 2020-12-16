d1424d3160124e141a9898ac43a86ecd589d6178
Roland <57312653+Landon-12@users.noreply.github.com>
2020-12-16 20:29:19 +0400

    Project

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('.reviews-slider').slick({
+        prevArrow: '<button type="button" class="reviews-slider__prev reviews__prev-btn"><img src="images/reviews/left-arrow.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="reviews-slider__next reviews__next-btn"><img src="images/reviews/right-arrow.svg" alt=""></button>'
+    });
+});
